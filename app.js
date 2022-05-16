20f05a8a6be6b3bbb47bd4cb19e3ee2093f1d3a3
dirk-woehr <51853046+dirk-woehr@users.noreply.github.com>
2022-05-16 23:12:53 +0200

    Sets up initial server

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const port = 5000;
+
+let requests = ["a", "b"];
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (_req, res) => {
+  res.json({
+    name: "waves-shared-session-server",
+    varsion: 0.1,
+    requests,
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server should be running at http://localhost:" + port);
+});
